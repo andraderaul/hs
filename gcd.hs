2b77a951935d9d24085269038674787f13cb441e
Raul Andrade <raul10.3@hotmail.com>
2022-03-31 23:14:46 -0300

    feat: add gcd

---
+++ gcd.hs
@@ -0,0 +1,17 @@
+module Main where
+
+gcd' :: Integral a => a -> a -> a
+gcd' n m
+    | n == m = n
+    | m == 0 = n
+    | n == 0 = m
+    | m >= n = gcd' n (m - n)
+    | n >= m = gcd' (n - m) m
+    | otherwise = 1
+
+main = do
+  input <- getLine
+  print . uncurry gcd' . listToTuple . convertToInt . words $ input
+ where
+  listToTuple (x:xs:_) = (x,xs)
+  convertToInt = map (read :: String -> Int)
