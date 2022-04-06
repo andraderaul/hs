338b1ebe4ea2a6668a5c7381d44813bbc774aa7a
Raul Andrade <raul10.3@hotmail.com>
2022-04-06 00:16:20 -0300

    feat: add doubles function

---
+++ doubles.hs
@@ -0,0 +1,11 @@
+doubleMe :: Num a => a -> a
+doubleMe x = x + x
+
+doubleUs :: Num a => a -> a -> a
+doubleUs x y = doubleMe x + doubleMe y
+
+doubleSmallNumber :: (Ord a, Num a) => a -> a
+doubleSmallNumber x =
+  if x > 100
+    then x
+    else x * 2
--- len.hs
+++ len.hs
@@ -1,3 +1,6 @@
 len :: [a] -> Int
 len [] = 0
 len (_ : lst) = 1 + len lst
+
+len' :: Num a => [t] -> a
+len' xs = sum [1 | _ <- xs]
